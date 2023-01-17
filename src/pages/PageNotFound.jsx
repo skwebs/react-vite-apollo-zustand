cc8c8c2cb0c128b1f9efb4244c278ef005a0b8aa
Satish Kumar Sharma <skwebserv@gmail.com>
2023-01-17 21:00:51 +0530

    remove some extra files and imroved PageNotFound

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -27,19 +27,19 @@ const PageNotFound = () => {
 
                     <div className='flex justify-center items-center'>
 
-                        <div className='text-6xl font-semibold text-indigo-600 px-6 border-r-2'>
+                        <div className='text-6xl font-semibold text-indigo-600 px-6'>
                             404
                         </div>
-                        <div className='font-semibold px-6'>
+                        <div className='border-l-2 font-semibold px-6'>
                             <div className='text-2xl md:text-4xl text-gray-800'>
                                 Page Not Found
                             </div>
                             <p className='text-xs md:text-base text-gray-400'>Please check the URL in address bar and try again.</p>
                         </div>
                     </div>
-                    <div className='mt-10'>
+                    <div className='mt-10 flex justify-center'>
                         <NavLink className="mx-2 px-4 py-2 rounded-lg bg-indigo-600 hover:bg-indigo-700 hover:text-white text-white font-semibold" to={`/`} >Go to Home</NavLink>
-                        <NavLink className="px-4 py-2 mt-10 rounded-lg bg-indigo-100 text-indigo-600 mx-2 font-semibold" to={`/contact`} >Contact Us</NavLink>
+                        <NavLink className="px-4 py-2 rounded-lg bg-indigo-100 text-indigo-600 mx-2 font-semibold" to={`/contact`} >Contact Us</NavLink>
                     </div>
                     {/* <div className='mt-5 text-indigo-600'>You will redirect to <strong>Home Page</strong> in {cd} seconds</div> */}
                 </ConstantWideLayout>
